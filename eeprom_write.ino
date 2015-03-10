50d6aa3d503a5a882beb6c22841bf321156a38e5
lovelynannanqiqi <arduino001@163.com>
2015-03-10 11:33:22 +0800

    更新
    增加
    readEEPByPart（）
    initIoMode();
    initSetIO();
    函数

--- eeprom_write.ino
+++ eeprom_write.ino
@@ -10,7 +10,8 @@
 
 // the current address in the EEPROM (i.e. which byte
 // we're going to write to next)
-#define IO_Number 10
+#define IO_START_PIN 4
+#define IO_NUMBER 10
 #define IO_BitLength 1
 
 #define IO_MODE_BeginEEPromAddress 0
@@ -24,12 +25,15 @@ void setup()
     Serial.begin(9600);
     //writeEEP("meme$",'$');
     
-    getUserCommand("#IOMODESET,B21122000000011$");
-    getUserCommand("#IOSET,P21122000000011$");
+    getUserCommand("#IOMODESET,1111111111$");
+    getUserCommand("#IOSET,0000000000$");
     String n=readEEP(512);
     Serial.println(n);
     
-    
+    Serial.println(readEEPByPart(IO_MODE_BeginEEPromAddress,IO_NUMBER));
+    Serial.println(readEEPByPart(IO_SET_BeginEEPromAddress,IO_NUMBER));
+    initIoMode(readEEPByPart(IO_MODE_BeginEEPromAddress,IO_NUMBER));
+    initSetIO(readEEPByPart(IO_SET_BeginEEPromAddress,IO_NUMBER));
     
 }
 
@@ -38,6 +42,25 @@ void loop()
 
 }
 
+void initIoMode(String iomodes)
+{
+   int iomodes_pointer=0;
+   for(int ioPin=IO_START_PIN;ioPin<IO_START_PIN+IO_NUMBER;ioPin++)
+   {     
+       pinMode(ioPin,String(iomodes.charAt(iomodes_pointer)).toInt());
+       iomodes_pointer+=1;
+   }
+}
+void initSetIO(String ioset)
+{
+   int ioset_pointer=0;
+   for(int ioPin=IO_START_PIN;ioPin<IO_START_PIN+IO_NUMBER;ioPin++)
+   {
+       Serial.println(ioset.charAt(ioset_pointer));
+       digitalWrite(ioPin,String(ioset.charAt(ioset_pointer)).toInt());
+       ioset_pointer+=1;
+   }
+}
 
 //info fromat "#COMMANDparameter$";
 void getUserCommand(String info)
@@ -46,20 +69,20 @@ void getUserCommand(String info)
   String command=info.substring(0,info.indexOf(","));
    int commandLen=String(command).length()+1;
   if (command ==IO_MODE_SET) {
-     writeEEP(info.substring(info.indexOf(IO_MODE_SET)+commandLen,info.indexOf("$")),IO_MODE_BeginEEPromAddress);
+     writeEEP(info.substring(info.indexOf(IO_MODE_SET)+commandLen,info.indexOf("$")),IO_MODE_BeginEEPromAddress,IO_NUMBER);
   }
   if (command ==IO_SET){
-     writeEEP(info.substring(info.indexOf(IO_SET)+commandLen,info.indexOf("$")),IO_SET_BeginEEPromAddress);
+     writeEEP(info.substring(info.indexOf(IO_SET)+commandLen,info.indexOf("$")),IO_SET_BeginEEPromAddress,IO_NUMBER);
   }
  
   
 }
 
-void writeEEP(String info,int startAddress)
+void writeEEP(String info,int startAddress,int writeLen)
 {
-   int len=info.length();
+
    
-   for(int i=0;i<len;i++)
+   for(int i=0;i<writeLen;i++)
    {
        EEPROM.write(startAddress,info.charAt(i));
        startAddress++;
@@ -100,4 +123,13 @@ String readEEP(int num)
   }  
   return result;
     
+}
+String readEEPByPart(int addr_begin,int length)
+{
+   String result;
+   int len=addr_begin+length;
+   for(;addr_begin<len;addr_begin++){
+       result+=String(char(EEPROM.read(addr_begin)));
+   }
+   return result;
 }
